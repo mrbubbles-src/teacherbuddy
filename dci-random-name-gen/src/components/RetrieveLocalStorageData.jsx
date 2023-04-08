b8623236ef33c90686854c0913f158d42ffaa4ff
mrbubbles-src <feaver5683@gmail.com>
2023-04-08 17:23:26 +0200

    removed from standard locations

--- dci-random-name-gen/src/components/RetrieveLocalStorageData.jsx
+++
@@ -1,9 +0,0 @@
-export default function RetrieveLocalStorageData() {
-    let studentNamesArr = [];
-    for (let i = 0; i < localStorage.length; i++) {
-        const localStorageNameKey = localStorage.key(i);
-        const studentName = localStorage.getItem(localStorageNameKey);
-        studentNamesArr.push(studentName);
-    }
-    return studentNamesArr;
-}
