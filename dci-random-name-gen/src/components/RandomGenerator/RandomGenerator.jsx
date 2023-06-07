31a8959c7d6a1dd292c4a2a27dd78f5c61104b07
mrbubbles-src <feaver5683@gmail.com>
2023-06-07 15:59:50 +0200

    added span

--- dci-random-name-gen/src/components/RandomGenerator/RandomGenerator.jsx
+++ dci-random-name-gen/src/components/RandomGenerator/RandomGenerator.jsx
@@ -6,8 +6,7 @@ let studentNamesArr = RetrieveLocalStorageData();
 export default function RandomGenerator() {
     const nameOutputField = document.getElementById("name");
     if (studentNamesArr.length === 0) {
-        nameOutputField.innerHTML =
-            "No more names! Press 'Reset Generator' to start generating again!";
+        nameOutputField.innerHTML = `No more names! Press <span class="orange-span">'Reset Generator'</span> to start generating again!`;
         document.getElementById("generate").style.display = "none";
     } else {
         let student = Math.floor(Math.random() * studentNamesArr.length);
