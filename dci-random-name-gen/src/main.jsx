7ba1756ab60d87e815fe5d012365b10babbbe9da
mrbubbles-src <feaver5683@gmail.com>
2023-06-07 15:52:46 +0200

    removed redundent imports

--- dci-random-name-gen/src/main.jsx
+++ dci-random-name-gen/src/main.jsx
@@ -1,10 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import "./assets/fonts/Poppins/Poppins-Regular.ttf";
-import "./assets/fonts/Poppins/Poppins-Light.ttf";
-import "./assets/fonts/Poppins/Poppins-Bold.ttf";
-import "./index.scss";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
