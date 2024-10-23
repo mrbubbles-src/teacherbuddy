ec7ec663ed3a1d7627a93124a2144cc54e77f7ed
mrbubbles-src <manuel.fahrenholz@digitalcareerinstitute.org>
2024-10-23 16:17:21 +0200

    Refactor saveToLocalStorage utility functions

--- utils/saveToLocalStorage/saveToLocalStorage.ts
+++ utils/saveToLocalStorage/saveToLocalStorage.ts
@@ -1,6 +1,8 @@
 import { ISaveToLocalStorage } from '@/lib/types';
 
-export const saveToLocalStorage = (state: ISaveToLocalStorage) => {
+export const saveStudentsToLocalStorage = (state: ISaveToLocalStorage) => {
   localStorage.setItem('studentNames', JSON.stringify(state.studentNames));
+};
+export const saveQuizToLocalStorage = (state: ISaveToLocalStorage) => {
   localStorage.setItem('quizQuestions', JSON.stringify(state.quizQuestions));
 };
