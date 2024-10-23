0b826c780fcbb3f8c67cbd170f616de562d603bf
mrbubbles-src <manuel.fahrenholz@digitalcareerinstitute.org>
2024-10-23 13:33:03 +0200

    Refactor Footer component to update author information and add documentation link styles

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -4,6 +4,7 @@ const Footer = () => {
       <p>
         Created by{' '}
         <a
+          className="font-semibold text-brand-primary underline underline-offset-2"
           href="https://github.com/mrbubbles-src"
           target="_blank"
           rel="noopener noreferrer">
@@ -11,6 +12,7 @@ const Footer = () => {
         </a>
         . Check out the documentation{' '}
         <a
+          className="font-semibold text-brand-secondary underline underline-offset-2"
           href="https://github.com/mrbubbles-src/random-student-name-generator"
           target="_blank"
           rel="noopener noreferrer">
