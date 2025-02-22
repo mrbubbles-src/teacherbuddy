81ce88afe4976d3d338c3fc99d74bc8f1ee5dcc2
mrbubbles-src <manuel_fahrenholz@web.de>
2025-02-22 21:14:14 +0100

    feat: integrate ClearAllStudents and RemoveSingleStudent components into DisplayStudents

--- components/DisplayStudents/DisplayStudents.tsx
+++ components/DisplayStudents/DisplayStudents.tsx
@@ -1,9 +1,16 @@
 'use client';
 
 import { useTeacherBuddy } from '@/context/TeacherBuddyContext';
+import RemoveSingleStudent from '../RemoveSingle/RemoveSingleStudent';
+import ClearAllStudents from '../ClearAll/ClearAllStudents';
 
 const DisplayStudents = () => {
   const { state } = useTeacherBuddy();
+
+  if (!state) {
+    return null; // or handle the undefined state case appropriately
+  }
+
   const students: Array<string> = state.studentNames;
 
   // Sort the student names alphabetically
@@ -11,8 +18,20 @@ const DisplayStudents = () => {
 
   return (
     <>
-      {sortedStudents &&
-        sortedStudents.map((student, index) => <p key={index}>{student}</p>)}
+      <ClearAllStudents />
+      <section
+      // className="border-3 flex h-[100vh] gap-3"
+      >
+        {sortedStudents &&
+          sortedStudents.map((student, index) => (
+            <p
+              // className="rounded-2xl border border-brand-primary p-2"
+              key={index}>
+              {student}
+              {<RemoveSingleStudent student={student} />}
+            </p>
+          ))}
+      </section>
     </>
   );
 };
