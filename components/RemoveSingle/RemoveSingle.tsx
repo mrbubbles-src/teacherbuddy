754907b854f6706994d7add5ea55cbd979144389
mrbubbles-src <manuel_fahrenholz@web.de>
2025-03-23 19:45:54 +0100

    feat: enhance RemoveSingle component with Font Awesome trash icon and improved styling

--- components/RemoveSingle/RemoveSingle.tsx
+++ components/RemoveSingle/RemoveSingle.tsx
@@ -1,4 +1,6 @@
 import { useTeacherBuddy } from '@/context/TeacherBuddyContext';
+import { faTrashCan } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 const RemoveSingle = ({
   typeAction,
@@ -14,9 +16,9 @@ const RemoveSingle = ({
 
   return (
     <span
-      className="ml-5 cursor-pointer border-2 border-red-500 p-1 text-xs"
+      className="ml-5 cursor-pointer rounded-lg border-2 border-red-500 p-1 text-xs text-red-500"
       onClick={removeHandler}>
-      ❌
+      <FontAwesomeIcon className="" icon={faTrashCan} />
     </span>
   );
 };
