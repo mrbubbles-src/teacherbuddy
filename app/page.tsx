35840a055947c8379bad10920841897d3b2a0078
mrbubbles-src <manuel.fahrenholz@mrbubbles-src.dev>
2026-02-05 21:56:38 +0100

    Add logo to dashboard page and update layout
    - Integrated the TeacherBuddy logo into the dashboard page, enhancing brand visibility.
    - Updated the layout to include the logo above the DashboardCards component, improving the overall user interface.
    - Added new logo image files to the public images directory.

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,23 @@
-import DashboardCards from "@/components/dashboard/dashboard-cards"
+import Image from 'next/image';
+
+import DashboardCards from '@/components/dashboard/dashboard-cards';
+import Logo from '@/public/images/teacherbuddy-logo.png';
 
 export default function Page() {
   return (
-    <DashboardCards />
-  )
+    <>
+      <section className="justify-self-center mb-5 max-w-8/12">
+        <Image
+          src={Logo}
+          alt="TeacherBuddy Logo"
+          width={895}
+          height={372}
+          preload={true}
+          placeholder="blur"
+          blurDataURL={Logo.blurDataURL}
+        />
+      </section>
+      <DashboardCards />
+    </>
+  );
 }
